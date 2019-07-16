0bedcf766b2febaa1ec751e5293ffa2149c28aab
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-16 10:09:50 -0700

    new changes

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-import isYes from './is-yes.js';
 
-const quiz = document.getElementById('quiz');
-const score = document.getElementById('quiz-outcome');
 
-quizButton.onclick = function( {
-    alert('Hello, thanks for taking quiz!')
-})
+const quizButton = document.getElementById('quiz');
+// const score = document.getElementById('quiz-outcome');
+
+quizButton.onclick = function() {
+    alert('Hello, thanks for taking quiz!');
+};
+// const confirmed = confirm('Lets begin');
+// if(confirmed === false) {
+//     return;
+// }
+// // function(firstName) {
+//     let firstName=()
+//     alert( "What is your first name?")
+    
+// }
+// function(lastName) {
+//     let lastName=()
+//     alert( "What is your last name")
+// }
+// function(sayHello) {
+//     let sayHello= "Hello"
+//     alert(('Hello'), + ('firstName'), + ('lastName'))
+// }
+let yes = 0;
+const playPool = promt('Do I enjoy playing pool?');
+function ((correctYes) = true) {
+    
+}
+
+
+
+// const correctPlaypool = true;
+// if(correctYes);
+// yes + 1;
