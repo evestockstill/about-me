89009fed02d9feba8c51fd5c6778ad44396af10a
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-16 00:06:13 -0700

    html/css/start js

---
+++ app.js
@@ -0,0 +1,8 @@
+import isYes from './is-yes.js';
+
+const quiz = document.getElementById('quiz');
+const score = document.getElementById('quiz-outcome');
+
+quizButton.onclick = function( {
+    alert('Hello, thanks for taking quiz!')
+})
---
+++ src/is-yes.js
@@ -0,0 +1,12 @@
+function isYes(text) {
+    text = text.toLowerCase();
+    if(text === 'banana') {
+        return true;
+    }
+    else if(text === 'nana') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
