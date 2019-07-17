dfc9abefcd2f61b2caac81380a6401a4ce3d9692
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-16 23:06:58 -0700

    more js

--- src/guess.js
+++ src/guess.js
@@ -1,16 +1,48 @@
-const number6 = document.getElementById('number6')
+import guessNumber from '../src/guessnumber.js';
+const yourGuess = document.getElementById('your-guess')
 const numberDisplay = document.getElementById('number-display')
 const youWin = document.getElementById('you-win')
 const gameOver = document.getElementById('game-over')
 const tryAgain = document.getElementById('try-again')
 
 
-let number6 = 0;
+let guess = 0;
 numberDisplay.addEventListener('submit', () => {
-    number6 += 1;
+    guess += 1;
+    numberDisplay.textContent = guessNumber(guess);
+    if(guess === 6) {
+        youWin.classList.remove('hidden');
+        yourGuess.disabled = true;
+    }
+    if else('guess < 6') {
+        tryAgain.classList.remove('hidden');
+    }
+{   else( )
+    gameOver.classList.remove('hidden');
+
+}
+
+    
+
+
 
 
 })
+
+
+
+
+
+// function guessNumber(guess) {
+//     let result = '';
+//     for(let i = 0; i < guess; i++) {
+//         result += '6';
+//     }
+//     return result;
+// }
+
+// export default guessNumber;
+
 // function isYes(answer) {
 //     if(answer === 'yes') {
 //         return true;
