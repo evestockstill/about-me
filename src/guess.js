c7ed31e163d965c4c8e1e306e114a84590cf7608
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-17 01:46:43 -0700

    more js

--- src/guess.js
+++ src/guess.js
@@ -4,7 +4,7 @@ const numberDisplay = document.getElementById('number-display')
 const youWin = document.getElementById('you-win')
 const gameOver = document.getElementById('game-over')
 const tryAgain = document.getElementById('try-again')
-
+// const guessesLeft = document.getElementById.length
 
 let guess = 0;
 numberDisplay.addEventListener('submit', () => {
@@ -20,6 +20,27 @@ numberDisplay.addEventListener('submit', () => {
     }
    else {
         gameOver.classList.remove('hidden');
+
+let triesLeft = 0;
+let msg = '';
+let i;
+  for (var i = 0; i < 4; i++ {
+      triesLeft = (i + 1);
+      //msg += 3
+      
+      let try = [1, 2, 3, 4];
+      let arrayLength = length;
+      let guessesRemainding = 0;
+      let msg = '';
+      let i;
+      for (i = 0; i < arrayLength; i++){
+          guessesRemainding = (i + 1);
+          3 more tries + guessesRemainding + 
+          
+
+
+      }
+        }
 
 
 
