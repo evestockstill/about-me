945e94843a1e33681cc3e386318523c2b073017c
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-18 11:29:08 -0700

    starting js

--- src/rock.js
+++ src/rock.js
@@ -1,4 +1,16 @@
-const winsScore = 0;
-const lossesScore = 0;
-const winsScoreSpan = document.getElementById(win-score);
-const lossesScoreSpan = document.getElementById(loss-score);
+import shoot from './get-throw.js';
+const shootButton = document.getElementById('shoot-button');
+const message = document.getElementById('message');
+const winCount = document.getElementById('win-count');
+const lossCount = document.getElementById('loss-count');
+
+let wins = 0;
+let losses = 0;
+
+shootButton.addEventListener('click', () => {
+    const side = shoot();
+    rockImage.classList.remove('invisable');
+    const src = 'assets/sides' + side + 'jpg';
+    rockImage.src = src;
+    const sideButton = document.querySelector('input:checked');
+})
