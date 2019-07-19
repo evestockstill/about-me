b4e364ec6dd14d6bff6f24f8d98f5c8bdc583f6f
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-07-18 22:24:07 -0700

    done refactoring

--- src/rock.js
+++ src/rock.js
@@ -1,24 +1,24 @@
 import shoot from './get-throw.js';
+
+
 const shootButton = document.getElementById('shoot-button');
 const message = document.getElementById('message');
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
 const rockImage = document.getElementById('rock-image');
 
-
 let wins = 0;
 let losses = 0;
 
 shootButton.addEventListener('click', () => {
     const hand = shoot();
-    rockImage.classList.remove('invisible');
-    const src = 'assets/' + hand + '.png';
-    rockImage.src = src;
-    const userButton = document.querySelector('input:checked');
-
-    const tie = userButton.value === hand;
-    const win = userButton.value === 'rock' && hand === 'paper' || userButton.value === 'paper' && hand === 'rock' || userButton.value === 'scissors' && hand === 'paper';
-    const loss = userButton.value === 'paper' && hand === 'rock' || userButton.value === 'rock' && hand === 'paper' || userButton.value === 'paper' && hand === 'scissors';
+    computerImage(hand);
+    const usersChoice = fetchUsersChoice();
+    
+    const tie = usersChoice.value === hand;
+    const win = usersChoice.value === 'rock' && hand === 'scissors' || usersChoice.value === 'paper' && hand === 'rock' || usersChoice.value === 'scissors' && hand === 'paper';
+    const loss = usersChoice.value === 'paper' && hand === 'scissors' || usersChoice.value === 'rock' && hand === 'paper' || usersChoice.value === 'scissors' && hand === 'rock';
+    
     if(win) {
         message.textContent = 'You Win!!';
         wins++;
@@ -28,9 +28,19 @@ shootButton.addEventListener('click', () => {
         message.textContent = 'You Lost!!';
         losses++;
         lossCount.textContent = losses;
-
     }
     else if(tie) {
         message.textContent = 'We tied, shoot again';
     }
 });
+
+function computerImage(hand){
+    const src = 'assets/' + hand + '.png';
+    rockImage.classList.remove('invisible');
+    rockImage.src = src;
+}
+
+function fetchUsersChoice() {
+    const userButton = document.querySelector('input:checked');
+    return userButton.value;
+}
