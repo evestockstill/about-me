a04685d4bdea57222c47214264e6ad76766ebe67
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-19 19:49:41 -0700

    trying to test

--- test/guessNumber.test.js
+++ test/guessNumber.test.js
@@ -1,29 +1,29 @@
 import compareNumbers from '../src/guessnumber.js';
 
 const test = QUnit.test;
-
 test('compare numbers returns 0 when numbers are equal', (assert) => {
     // Arrange
     const guess = 6;
     const correctNumber = 6;
     const expected = 0;
-
-    // Act
     const result = compareNumbers(guess, correctNumber);
+   assert.equal(result, expected);
 
-    // Assert
-    assert.equal(result, expected);
-});
 
-test('compare numbers returns -1 when guessed number too low', (assert) => {
+  test('compare numbers returns -1 when guessed number too low', (assert) => {
     // Arrange
-    const guess = 0;
+    const guess = 1;
     const correctNumber = 6;
     const expected = -1;
-
-    // Act
     const result = compareNumbers(guess, correctNumber);
-
-    // Assert
     assert.equal(result, expected);
-});
+
+test('compare numbers returns +1 when guessed number too low', (assert) => {
+
+    const guess = 7;
+    const correctNumber = 6;
+    const expected = +1;
+    assert.equal(result, expected);
+})
+
+}
