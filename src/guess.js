8e72706532750774f8dfb3b98cff9230015d7422
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-22 08:21:23 -0700

    try-connect

--- src/guess.js
+++ src/guess.js
@@ -1,4 +1,4 @@
-import compareNumbers from '../src/guessnumber.js';
+import compareNumbers from './src/guessnumber.js';
 const press = document.getElementById('press');
 const triesLeftDisplay = document.getElementById('tries-left');
 const youWin = document.getElementById('you-win');
